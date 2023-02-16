d617f8d0e54ad432f625c75fdd0af37a9028a13c
AnatolYarmolovich <yarmolovich1986@gmail.com>
2023-02-16 10:31:14 +0100

    C-lectures-tasks: Mario more implemented

--- C/Mario-More/main.c
+++ C/Mario-More/main.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
+#include "../cs50lib/cs50.h"
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int getHeightPrompt(void);
+void buildWall(int height);
+
+int main(void)
+{
+    //Create getter for height
+    int height = getHeightPrompt();
+
+    //Create task handler
+    buildWall(height);
+}
+
+int getHeightPrompt(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    return  height;
+}
+
+void buildWall(int height)
+{
+
+    for (int row = 0; row < height; ++row)
+    {
+        for (int space = height - row - 1; space > 0; --space)
+        {
+            printf(" ");
+        }
+
+        for (int leftPile = height - row - 1; leftPile < height; ++leftPile)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int rightPile = 0; rightPile < row + 1; ++rightPile)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
